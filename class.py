4fdd318dc2d254b5bf6c89fb8f4ad21938cd2cd1
gagaero <49010114+gagaero@users.noreply.github.com>
2019-05-20 08:13:50 -0300

    ss

--- class.py
+++ class.py
@@ -1,39 +1,13 @@
-class Money  {
+abstract class Money  {
    protected int amount;
    
-   public boolean equals(Object object)  {
-      Money money = (Money) object;
-      return amount == money.amount;
-   }   
-}
-
-
-class Money.Dollar {
-   int amount;
-   Dollar(int amount) {
-      this.amount= amount;
+   static Money dollar(int amount)  {
+      return new Dollar(amount);
    }
-   void times(int multiplier) {
-      amount= amount * multiplier;
-   }
-}	
-
-class Money.Franc {   
-   private int amount;					
-   Franc(int amount) {      
-      this.amount= amount;
-    }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
-    public boolean equals(Object object) {					
-       Franc franc = (Franc) object;      
-       return amount == franc.amount;					
-     }					
+   
+   static Money franc(int amount) {
+      return new Franc(amount);
+    }
 }
 
 
-public boolean equals(Object object)  {
-   Dollar dollar = (Dollar) object;
-   return amount == dollar.amount;
-}
